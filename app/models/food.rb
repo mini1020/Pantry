b8883fb1148d0a5bc500c8dcf322c03aac22d343
RumiNakamura <krw.psh2196@gmail.com>
2021-03-07 17:22:22 +0900

    Storageモデル、Foodモデルのバリデーション確認完了

--- app/models/food.rb
+++ app/models/food.rb
@@ -4,11 +4,12 @@ class Food < ApplicationRecord
   validates :name, presence: true, null: false,
                    length: { maximum: 40 }
   validates :quantity, presence: true, null: false,
-                   length: { in: 1..30 }
-  validates :purchase, null: false
+                       numericality: { greater_than: 1, less_than: 30 }
+  validates :purchase, presence: true, null: false
   validate :purchasedate_earlier_than_not_today_possible,
            :expirationdate_before_inputdate_is_not_possible,
-           :noticedate_before_inputdate_and_expirationdate_is_not_possible,
+           :noticedate_before_inputdate_is_not_possible,
+           :noticedate_after_expirationdate_is_not_possible,
            :no_expirationdate_cannot_enter_noticedate
            
   # 入力日より先の購入日は入力不可
@@ -25,10 +26,16 @@ class Food < ApplicationRecord
     end
   end
 
-  # 入力日以前前且つ使い切り期限より先のお知らせ日は入力不可
-  def noticedate_before_inputdate_and_expirationdate_is_not_possible
-    if notice.present? && notice <= Date.today && notice > expiration
-      errors.add(:notice, "本日以前、且つ使い切り期限より先の日付は入力できません")
+  # 入力日以前のお知らせ日は入力不可
+  def noticedate_before_inputdate_is_not_possible
+    if notice.present? && notice <= Date.today
+      errors.add(:notice, "本日以前の日付は入力できません")
+    end
+  end
+  # 使い切り期限より先のお知らせ日は入力不可
+  def noticedate_after_expirationdate_is_not_possible
+    if notice.present? && notice > expiration
+      errors.add(:notice, "使い切り期限より先の日付は入力できません")
     end
   end
 
--- app/models/storage.rb
+++ app/models/storage.rb
@@ -2,6 +2,6 @@ class Storage < ApplicationRecord
   belongs_to :user
   has_many :foods, dependent: :destroy
 
-  validates :name, presence: true, null: false,
+  validates :place, presence: true, null: false,
                    length: { maximum: 20 }, uniqueness: true
 end
