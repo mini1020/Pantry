0bf3e8d5d9d73b89b4d20c4bee63f6851ff0a30d
RumiNakamura <krw.psh2196@gmail.com>
2021-03-14 14:20:28 +0900

    show.html.slimファイルを作成、ルーティングを編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
 
   root "homes#top"
-  get 'foods/new'
-  get 'storages/new'
+  # get 'foods/new'
+  # get 'storages/new'
   devise_for :users, controllers: {
     omniauth_callbacks: "omniauth_callbacks"
   }
+  
+  resources :users, only: [:show] do
+    resources :storages do
+      resources :foods
+    end
+  end
   
 end
