c5306424bc78a4d5040387cc8c7c134dc5d458f5
RumiNakamura <krw.psh2196@gmail.com>
2021-03-30 07:41:38 +0900

    ユーザー情報表示の権限を追加、管理者ログイン後のヘッダーのビューを編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,18 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit(:account_update, keys: [:uname])
     end
 
-    private
-      def set_user
-        @user = User.find(params[:id])
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def admin_or_correct_user
+      @user = User.find(params[:id])
+      unless current_user.admin?
+        unless @user == current_user
+          flash[:danger] = "アクセス権限がありません。"
+          redirect_to root_url 
+        end
       end
+    end
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :authenticate_user!, only: [:edit, :update, :destroy]
 
   # GET /resource/sign_up
   def new
