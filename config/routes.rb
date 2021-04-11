9cdf6345756e7b0ed3b9f87ee8e269f8aba69344
RumiNakamura <krw.psh2196@gmail.com>
2021-04-12 07:35:34 +0900

    管理者用のユーザー情報編集ページのビューを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,23 @@
 Rails.application.routes.draw do
   root "homes#top"
 
-  devise_for :admins, controllers: {
-    registrations: "users/registrations",  
+  devise_for :admins, controllers: {  
     sessions: "admins/sessions"
   }
     namespace :admins do
-      resources :users, only: [:index, :edit, :update, :destroy]
+     resources :users, only: [:index, :edit, :update, :destroy]
     end
 
   devise_for :users, controllers: {
-    registrations: "users/registrations",
-    sessions: "users/sessions",
-    omniauth_callbacks: "omniauth_callbacks"
+    registrations: "devise/registrations",
+    sessions: "devise/sessions",
+    omniauth_callbacks: "devise/omniauth_callbacks"
   }
-    resources :users, only: [:show] do
-      resources :storages do
-        resources :foods
-      end
-    end
+    resources :users, only: [:show] # do
+    #   resources :storages do
+    #     resources :foods
+    #   end
+    # end
 
   # devise_scope :user do
   #   get "users/:id/edit", to: "users/registrations#edit", as: :edit_other_user_registration
