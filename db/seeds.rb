09894b41f5fb883630a2c222c57cf749e2b28d5e
RumiNakamura <krw.psh2196@gmail.com>
2021-04-20 06:49:54 +0900

    ビューの微調整完了 #22

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,6 @@ Admin.create!(
   email: "admin@email.com",
   password: "password")
 
-# Admin.find_or_create_by(id: 1) do |admin|
-#   admin.email = "admin@email.com"
-#   admin.password: "password"
-# end
-
 4.times do |n|
   User.create!(
     uname: "User#{n + 1}",
