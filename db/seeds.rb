506d721ea185b9f5d58692b52900d37cb8062e90
RumiNakamura <krw.psh2196@gmail.com>
2021-04-20 07:07:40 +0900

    サンプルユーザーを5人から10人に変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ Admin.create!(
   email: "admin@email.com",
   password: "password")
 
-4.times do |n|
+15.times do |n|
   User.create!(
     uname: "User#{n + 1}",
     email: "sample#{n + 1}@email.com",
