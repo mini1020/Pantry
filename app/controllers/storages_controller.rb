b7f07bd4d7bcc690cc64c2e2a587a42502b36d8d
RumiNakamura <krw.psh2196@gmail.com>
2021-04-23 07:45:59 +0900

    new、createアクションを編集、ストロングパラメータを追加、登録ページのビューを編集

--- app/controllers/storages_controller.rb
+++ app/controllers/storages_controller.rb
@@ -1,11 +1,24 @@
 class StoragesController < ApplicationController
-  
+
   def new
+    @user = User.find(params[:user_id])
     @storage = Storage.new
   end
 
   def create
-    
+    @user = User.find(params[:user_id])
+    @storage = Storage.new(storage_params)
+    if @storage.save
+      flash[:success] = "保管場所を登録しました。"
+      redirect_to user_storages_url
+    else
+      debugger
+      render :new
+    end
   end
 
+  private
+    def storage_params
+      params.require(:storage).permit(:place, :user_id)
+    end
 end
