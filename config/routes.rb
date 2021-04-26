852251db28fd35924b9feece9575a7e2871fe615
RumiNakamura <krw.psh2196@gmail.com>
2021-04-27 07:30:14 +0900

    食品登録機能の作成が完了 #26

--- config/routes.rb
+++ config/routes.rb
@@ -15,11 +15,12 @@ Rails.application.routes.draw do
   } 
 
   get "/users/:user_id/storages/foods/new", to: "foods#new", as: "new_user_storage_food"
+  post "/users/:user_id/storages/foods", to: "foods#create", as: "user_storage_food"
   get "/users/:user_id/storages/foods", to: "foods#index", as: "user_storage_foods"
 
   resources :users, only: [:show] do
     resources :storages, only: [:index, :new, :create, :edit, :update, :destroy] do
-      resources :foods, only: [:create, :edit, :update, :destroy]
+      resources :foods, only: [:edit, :update, :destroy]
     end
   end
 
