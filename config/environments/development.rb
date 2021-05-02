3b9947cc14ec0255796d1bcd70f070e9c09cba4b
RumiNakamura <krw.psh2196@gmail.com>
2021-05-02 10:21:16 +0900

    letter_opener_web gemを追加、development.rbにメール送信設定を追加、送信元メールアドレスを修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,18 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:                 587,
+    address:              "smtp.gmail.com",
+    domain:               "gmail.com"
+    user_name:            "Pantry0502@gmail.com",
+    password:             "portfolio0502",
+    authentication:       "login",
+    enable_starttls_auto: true
+  }
 
   config.action_mailer.perform_caching = false
 
