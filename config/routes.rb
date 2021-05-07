5a11785288401738916e5270e7bea65d38074d51
RumiNakamura <krw.psh2196@gmail.com>
2021-05-07 17:17:51 +0900

    2021_05_07 ルーティングを設定、カラムを追加

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,10 @@ Rails.application.routes.draw do
   } 
 
   resources :users, only: [:show] do
+    member do
+      get "edit_destroy_request"
+      patch "update_destroy_request"
+    end
     resources :storages, only: [:index, :new, :create, :edit, :update, :destroy]
   end
 
@@ -30,5 +34,7 @@ Rails.application.routes.draw do
   get "/users/:user_id/storages/:storage_id/foods/:id/edit", to: "foods#edit", as: "edit_user_storage_food"
   patch "/users/:user_id/storages/:storage_id/foods/:id", to: "foods#update", as: "update_user_storage_food"
   delete "/users/:user_id/storages/:storage_id/foods/:id/", to: "foods#destroy", as: "destroy_user_storage_food"
+
+
 
 end
