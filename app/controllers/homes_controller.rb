721f31fd46b31c23308c5507c1576b6ae87a60e5
RumiNakamura <krw.psh2196@gmail.com>
2021-06-04 12:42:50 +0900

    ログイン後トップページにアクセスできないようにするするメソッドを追加

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,17 @@
 class HomesController < ApplicationController
+before_action :user_or_admin_logged_in
 
   def top
   end
+
+  private
+    # ログイン後はトップページにアクセスさせない
+    def user_or_admin_logged_in
+      if admin_signed_in?
+        redirect_to admins_users_url
+      elsif user_signed_in?
+        redirect_to user_storage_foods_url(current_user)
+      end
+    end
   
 end
